3e4853c88dacfe85b374219052d411ca35fd87a1
programmer <myalkin.vlad@yandex.ru>
2024-03-30 15:46:16 +0300

    feat: Добавление вывода вложенных комментариев.

--- django_movie/movies/serializers.py
+++ django_movie/movies/serializers.py
@@ -19,12 +19,31 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class RecursiveSerializer(serializers.Serializer):
+    """Вывод рекурсивно children"""
+
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
+class FilterReviewListSerializer(serializers.ListSerializer):
+    """Фильтр коммментариев. Выводит только parents комментарии, убирает дублирование вложенных коментариев"""
+
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+
 class ReviewSerializer(serializers.ModelSerializer):
     """Вывод отзыва"""
+    # выводит все вложенные коментарии
+    children = RecursiveSerializer(many=True)
 
     class Meta:
+        list_serializer_class = FilterReviewListSerializer
         model = Review
-        fields = ('name', 'text', 'parent')
+        fields = ('name', 'text', 'children')
 
 
 class MovieDetailSerializer(serializers.ModelSerializer):
