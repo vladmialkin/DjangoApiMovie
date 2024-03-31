820dfccfe35d1b9597d2300e5369885a6e0ed06d
programmer <myalkin.vlad@yandex.ru>
2024-03-31 10:47:32 +0300

    feat: Добавление фильтров для списка фильмов.

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,13 +1,16 @@
 from .models import Movie, Actor
 from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer, CreateRatingSerializer, \
     ActorListSerializer, ActorDetailSerializer
-from .service import get_client_ip
+from .service import get_client_ip, MovieFilter
 from django.db import models
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class MovieListView(generics.ListAPIView):
     serializer_class = MovieListSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = MovieFilter
 
     def get_queryset(self):
         movies = Movie.objects.filter(draft=False).annotate(
